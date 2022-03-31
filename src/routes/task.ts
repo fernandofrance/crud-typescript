f764bb4d5cd5d77b8fcf698867d89a8c8df658e3
Luiz Fernando <root.luizfernando.dev@gmail.com>
2022-03-31 17:09:19 -0300

    Added route for update tasks

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 
 const router = express.Router();
 
-// Get tasklist
+    // Get tasklist
 router.get("/tasklist", (req: Request, res: Response) => {
     const path = "./src/tasks.json"
     fs.readFile(path, "utf8", (err, data) => {
@@ -18,9 +18,9 @@ router.get("/tasklist", (req: Request, res: Response) => {
             console.error(err);
         }
     });
-});
+})
 
-// Post task
+// Create task
 router.post("/addtask", (req: Request, res: Response) => {
     const body = req.body;
     fs.readFile("./src/tasks.json", "utf8", (err, buffer) => {
@@ -32,7 +32,7 @@ router.post("/addtask", (req: Request, res: Response) => {
         let data = JSON.parse(buffer)
         data.tasks.push(body)
 
-        fs.writeFile("./src/tasks.json", JSON.stringify(data), err => {
+        fs.writeFile("./src/tasks.json", JSON.stringify(data, null, 2), err => {
             if (err) {
                 console.error(err)
                 return res.status(500).json({ "error": "Could not WRITE the file" })
@@ -41,6 +41,36 @@ router.post("/addtask", (req: Request, res: Response) => {
         })
     });
     
-});
+})
+
+// Update task
+router.put("/updatetask", (req: Request, res: Response) => {
+    const { identifier } = req.query;
+    const { taskName, taskDesc } = req.body;
+    
+    fs.readFile("./src/tasks.json", "utf8", (err, buffer) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ "error": "Could not READ the file" })
+        }
+        
+        const data = JSON.parse(buffer)
+        
+        for (let i = 0; i < data.tasks.length; i++) {
+            //console.log(data.tasks[i].taskName)
+            if (data.tasks[i].taskName == identifier) {
+                data.tasks[i].taskName = taskName
+                data.tasks[i].taskDesc = taskDesc
+                fs.writeFile("./src/tasks.json", JSON.stringify(data, null, 2), err => {
+                    if (err) {
+                        console.log(err)
+                        return res.status(500).json({ "error": "Could not WRITE the file" })
+                    }
+                })
+                return res.status(200).json({ "message": "Task updated"})
+            }
+        }
+    })
+})
 
 module.exports = router;
