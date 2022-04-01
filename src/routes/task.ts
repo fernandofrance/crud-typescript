726f6330cd457261b896698e369253da6e220d16
Luiz Fernando <root.luizfernando.dev@gmail.com>
2022-04-01 20:40:30 -0300

    Moved controller code to src/controllers/taskController.ts

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,101 +1,18 @@
-import express from "express";
-import fs from "fs";
-import { Request, Response } from "express";
+import { Router } from "express";
+import { taskController } from "../controllers/taskController";
 
-const router = express.Router();
+const taskRouter = Router();
 
-    // Get tasklist
-router.get("/tasklist", (req: Request, res: Response) => {
-    const path = "./src/tasks.json"
-    fs.readFile(path, "utf8", (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: "Could not read the file" });
-        }
-        try {
-            res.status(200).send(data);
-        } catch (err) {
-            console.error(err);
-        }
-    });
-})
+// Get tasklist
+taskRouter.get("/gettasks", taskController.getTasks);
 
 // Create task
-router.post("/addtask", (req: Request, res: Response) => {
-    const body = req.body;
-    fs.readFile("./src/tasks.json", "utf8", (err, buffer) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({ error: "Could not READ the file" });
-        }
-
-        let data = JSON.parse(buffer)
-        data.tasks.push(body)
-
-        fs.writeFile("./src/tasks.json", JSON.stringify(data, null, 2), err => {
-            if (err) {
-                console.error(err)
-                return res.status(500).json({ "error": "Could not WRITE the file" })
-            }
-            res.status(203).json({ "message": "task added" })
-        })
-    });
-    
-})
+taskRouter.post("/addtask", taskController.addTask);
 
 // Update task
-router.put("/updatetask", (req: Request, res: Response) => {
-    const { name } = req.query;
-    const { taskName, taskDescription } = req.body;
-    
-    fs.readFile("./src/tasks.json", "utf8", (err, buffer) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ "error": "Could not READ the file" })
-        }
-        
-        const data = JSON.parse(buffer)
-        
-        for (let i = 0; i < data.tasks.length; i++) {
-            if (data.tasks[i].taskName == name) {
-                data.tasks[i].taskName = taskName
-                data.tasks[i].taskDescription = taskDescription
-                fs.writeFile("./src/tasks.json", JSON.stringify(data, null, 2), err => {
-                    if (err) {
-                        console.log(err)
-                        return res.status(500).json({ "error": "Could not WRITE the file" })
-                    }
-                })
-                return res.status(200).json({ "message": "Task updated" })
-            }
-        }
-    })
-})
+taskRouter.put("/updatetask", taskController.updateTask);
 
 // Delete task
-router.delete("/deletetask", (req: Request, res: Response) => {
-    const { name } = req.query;
+taskRouter.delete("/deletetask", taskController.deleteTask);
 
-    fs.readFile("./src/tasks.json", "utf8", (err, buffer) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: "Could not READ the file" });
-        }
-
-        const data = JSON.parse(buffer)
-        for (let i = 0; i < data.tasks.length; i++) {
-            if (data.tasks[i].taskName == name) {
-                data.tasks.splice(i, 1)
-                fs.writeFile("./src/tasks.json", JSON.stringify(data, null, 2), err => {
-                    if (err) {
-                        console.log(err)
-                        return res.status(500).json({ "error": "Could not WRITE the file" })
-                    }
-                })
-                return res.status(200).json({ "message": "Task deleted" })
-            }
-        }
-    })
-})
-
-module.exports = router;
+module.exports = taskRouter;
